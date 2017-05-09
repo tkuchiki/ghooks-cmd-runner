ad38ca89da0d409701d2cd79f6600a9ea7a27441
tkuchiki <kuchiki.taku@gmail.com>
2017-05-09 11:09:18 +0900

    ver++

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ var (
 
 func main() {
 	kingpin.CommandLine.Help = "Receives Github webhooks and runs commands"
-	kingpin.Version("0.3.0")
+	kingpin.Version("0.3.1")
 	kingpin.Parse()
 
 	tmpConf := config{
