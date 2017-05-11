418b018813fe0f8dd1a0d73291d32fb18d8aef3d
mackee <macopy123@gmail.com>
2017-05-11 12:41:20 +0900

    change callback from clojure to hook method

--- config.go
+++ config.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/BurntSushi/toml"
 	"io/ioutil"
+
+	"github.com/BurntSushi/toml"
 )
 
 type config struct {
@@ -12,15 +13,6 @@ type config struct {
 	Logfile string `toml:"logfile"`
 	Pidfile string `toml:"pidfile"`
 	Hook    []hook
-}
-
-type hook struct {
-	Event          string   `toml:"event"`
-	Cmd            string   `toml:"command"`
-	Branch         string   `toml:"branch"`
-	IncludeActions []string `toml:"include_actions"`
-	ExcludeActions []string `toml:"exclude_actions"`
-	AccessToken    string   `toml:"access_token"`
 }
 
 type hooks struct {
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
+	"os"
+
 	"github.com/Konboi/ghooks"
 	"github.com/Sirupsen/logrus"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"os"
-	"sync"
-	"time"
 )
 
 type cmd struct {
@@ -74,99 +71,7 @@ func main() {
 		if h.Event == "" {
 			log.Fatal("event is required.")
 		}
-		hookingBranch := h.Branch
-		hooks.On(h.Event, func(payload interface{}) {
-			action := parseAction(payload)
-
-			if !includeActions(action, h.IncludeActions) {
-				log.Info(fmt.Sprintf("skipped action = %s (%s)", action, h.Event))
-				return
-			}
-
-			if excludeActions(action, h.ExcludeActions) {
-				log.Info(fmt.Sprintf("skipped action = %s (%s)", action, h.Event))
-				return
-			}
-
-			branch := parseBranch(payload)
-			var matched bool
-			matched, err = matchBranch(branch, hookingBranch)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if matched {
-				m := new(sync.Mutex)
-
-				var buf []byte
-				buf, err = json.Marshal(payload)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				m.Lock()
-
-				var g githubClient
-				if h.Event == "pull_request" && h.isNotBlankAccessToken() {
-					owner, repo, ref := parsePullRequestStatus(payload)
-					g = NewClient(owner, repo, ref, h.AccessToken)
-
-					g.pendingStatus()
-				}
-
-				var successF, failureF *os.File
-				var successDir, failureDir string
-				if h.Event == "pull_request" && h.isNotBlankAccessToken() {
-					successF, successDir, err = createTempFile()
-					if err != nil {
-						log.Error(err)
-						m.Unlock()
-						removeDirs(successDir)
-						return
-					}
-					os.Setenv("SUCCESS_TARGET_FILE", successF.Name())
-
-					failureF, failureDir, err = createTempFile()
-					if err != nil {
-						log.Error(err)
-						m.Unlock()
-						removeDirs(successDir, failureDir)
-						return
-					}
-					os.Setenv("FAILURE_TARGET_FILE", failureF.Name())
-				}
-
-				p := base64.StdEncoding.EncodeToString(buf)
-				err = runCmd(h.Cmd, p)
-
-				if h.Event == "pull_request" && h.isNotBlankAccessToken() && err == nil {
-					err = g.successStatus(readlineTempFile(successF))
-					removeDirs(successDir, failureDir)
-					if err != nil {
-						log.Error(err)
-						m.Unlock()
-						removeDirs(successDir, failureDir)
-						return
-					}
-				}
-
-				if err != nil {
-					if h.isNotBlankAccessToken() {
-						err = g.failureStatus(readlineTempFile(failureF))
-						removeDirs(successDir, failureDir)
-						if err != nil {
-							log.Error(err)
-						}
-					}
-					log.Error(err)
-					m.Unlock()
-					return
-				}
-
-				time.Sleep(1000 * time.Millisecond)
-				m.Unlock()
-			}
-		})
+		hooks.On(h.Event, h.callback)
 	}
 
 	hooks.Run()
